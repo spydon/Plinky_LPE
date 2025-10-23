025fdf75abf8ed5add377bcd66a924fe8774cf62
RJ-Eckie <121803687+RJ-Eckie@users.noreply.github.com>
2025-10-23 15:03:04 +0200

    Perf: Web editor data retrieval safety

--- sw/Core/Src/plinky/usb/web_editor.c
+++ sw/Core/Src/plinky/usb/web_editor.c
@@ -2,85 +2,119 @@
 #include "hardware/memory.h"
 #include "tusb.h"
 
-/* webusb wire format. 10 byte header, then data.
-u32 magic = 0xf30fabca
-u8 cmd // 0 = get, 1=set
-u8 idx // 0
-u8 idx2 // 0
-u8 idx3 // 0
-u16 datalen // in bytes, not including this header, 0 for get, 1552 for set */
-
-typedef struct WebUSBHeader { // 10 byte header
+typedef struct WebUSBHeader {
 	u8 magic[4];
-	u8 cmd;
-	u8 idx;
+	u8 cmd; // 0 = request to send, 1 = request to save
+	u8 idx; // id of the preset
 	union {
 		struct {
-			u16 offset_16;
-			u16 len_16;
+			u16 offset_16; // offset inside preset (points to a parameter)
+			u16 len_16;    // amount of data to send/expect
 		};
-		struct { // these are valid if magic3 is 0xcc
+		struct {
 			u32 offset_32;
 			u32 len_32;
 		};
 	};
 } __attribute__((packed)) WebUSBHeader;
 
-// state machine ticks thru these in order, more or less
-typedef enum WuState {
-	WU_MAGIC0,
-	WU_MAGIC1,
-	WU_MAGIC2,
-	WU_MAGIC3,
-	WU_RCV_HDR,
-	WU_RCV_DATA,
-	WU_SND_HDR,
-	WU_SND_DATA,
-} WuState;
+typedef enum EditorState {
+	MAGIC0,
+	MAGIC1,
+	MAGIC2,
+	MAGIC3,
+	RCV_HEADER,
+	RCV_DATA,
+	SEND_HEADER,
+	SEND_DATA,
+} EditorState;
 
-const static u8 magic[4] = {0xf3, 0x0f, 0xab, 0xca};
-const static u8 magic_32[4] = {0xf3, 0x0f, 0xab, 0xcb}; // 32 bit version
+#define HEADER_IS_32BIT (header.magic[3] == magic_32[3])
+#define DATA_SIZE (HEADER_IS_32BIT ? header.len_32 : header.len_16)
+#define DATA_OFFSET (HEADER_IS_32BIT ? header.offset_32 : header.offset_16)
 
-static WuState state = WU_MAGIC0;   // current state
+const static u8 magic_16[4] = {0xf3, 0x0f, 0xab, 0xca};
+const static u8 magic_32[4] = {0xf3, 0x0f, 0xab, 0xcb};
+
+static EditorState state = MAGIC0;  // current state
 static WebUSBHeader header;         // header of current command
 static u8* data_buf = (u8*)&header; // buffer where we are reading/writing atm
 static u32 remaining_bytes = 1;     // how much left to read/write before state transition
+static u32 state_start = 0;         // usb timeout timer
 
-static inline bool is_wu_hdr_32bit(void) {
-	return header.magic[3] == magic_32[3];
-}
+u8 receiving_web_preset = false;
 
-static inline u32 wu_hdr_len(void) {
-	return is_wu_hdr_32bit() ? header.len_32 : header.len_16;
-}
-
-static inline u32 wu_hdr_offset(void) {
-	return is_wu_hdr_32bit() ? header.offset_32 : header.offset_16;
-}
-
-static void set_state(WuState new_state, u8* data, s32 len) {
+static void set_state(EditorState new_state) {
 	state = new_state;
-	data_buf = data;
-	remaining_bytes = len;
+	// start transfer timeout
+	if (state != MAGIC0)
+		state_start = millis();
+	// set up new state => set data_buf pointer and how many bytes to process
+	switch (state) {
+	case MAGIC0:
+	case MAGIC1:
+	case MAGIC2:
+	case MAGIC3:
+		data_buf = header.magic + state;
+		remaining_bytes = 1;
+		break;
+	case RCV_HEADER:
+		// data_buf is already at correct position
+		remaining_bytes = HEADER_IS_32BIT ? 10 : 6;
+		break;
+	case RCV_DATA:
+		load_preset(header.idx);
+		receiving_web_preset = true;
+		data_buf = (u8*)&cur_preset + DATA_OFFSET;
+		remaining_bytes = DATA_SIZE;
+		break;
+	case SEND_HEADER:
+		header.cmd = 1;
+		if (DATA_SIZE == 0) {
+			header.len_16 = sizeof(Preset) - DATA_OFFSET;
+			header.offset_16 = DATA_OFFSET;
+			header.magic[3] = magic_16[3];
+		}
+		data_buf = (u8*)&header;
+		remaining_bytes = HEADER_IS_32BIT ? 14 : 10;
+		break;
+	case SEND_DATA:
+		data_buf = preset_flash_ptr(header.idx) + DATA_OFFSET;
+		remaining_bytes = DATA_SIZE;
+		break;
+	}
 }
 
 void web_editor_reset(void) {
-	set_state(WU_MAGIC0, header.magic, 1);
+	state_start = 0;
+	receiving_web_preset = false;
+	set_state(MAGIC0);
 }
 
 void web_editor_frame(void) {
+	// transfer timeout
+	if (state_start != 0 && millis() - state_start > 5000)
+		web_editor_reset();
+	// run 1ms of web editor processing
 	u32 start_time = micros();
 	while (micros() - start_time < 1000) {
-		// process bytes
 		tud_task();
+
+		// first block: handle bytes in the current state
+
 		u32 handled_bytes = 0;
 		switch (state) {
 		// send bytes
-		case WU_SND_HDR:
-		case WU_SND_DATA:
+		case SEND_HEADER:
+		case SEND_DATA:
 			handled_bytes = tud_vendor_write(data_buf, remaining_bytes);
 			break;
 		// read bytes
+		case RCV_DATA:
+			// wait until flash has loaded the correct preset
+			if (preset_outdated())
+				continue;
+			// fall thru
 		default:
 			handled_bytes = tud_vendor_read(data_buf, mini(remaining_bytes, CFG_TUD_VENDOR_RX_BUFSIZE));
 			break;
@@ -97,35 +131,32 @@ void web_editor_frame(void) {
 		if (remaining_bytes)
 			continue;
 
-		// move to next state
+		// second block: all bytes are handled, transfer to new state
+
 		switch (state) {
 		// magic bytes
-		case WU_MAGIC0:
-		case WU_MAGIC1:
-		case WU_MAGIC2:
-		case WU_MAGIC3: {
+		case MAGIC0:
+		case MAGIC1:
+		case MAGIC2:
+		case MAGIC3: {
 			u8 m = header.magic[state];
-			// received incorrect magic byte
-			if (m != magic[state] && m != magic_32[state]) {
-				// received magic 0, manually set state to magic 1
-				if (m == magic[0]) {
-					set_state(WU_MAGIC1, header.magic + 1, 1);
-					break;
-				}
-				// reset to init state
-				web_editor_reset();
+			// incorrect magic byte
+			if (m != magic_16[state] && m != magic_32[state]) {
+				// received magic 0, move to magic 1
+				if (m == magic_16[0])
+					set_state(MAGIC1);
+				// other magic byte, reset to init state
+				else
+					web_editor_reset();
 				break;
 			}
-			state++;
-			remaining_bytes = 1;
-			// time to get rest of header
-			if (state == WU_RCV_HDR)
-				remaining_bytes = is_wu_hdr_32bit() ? 10 : 6;
+			// correct magic byte, progress
+			set_state(state + 1);
 			break;
 		}
-		// header received
-		case WU_RCV_HDR:
-			// only accept valid presets
+		// finished receiving header
+		case RCV_HEADER:
+			// only accept presets
 			if (header.idx >= NUM_PRESETS) {
 				web_editor_reset();
 				break;
@@ -133,35 +164,26 @@ void web_editor_frame(void) {
 			switch (header.cmd) {
 			// request to send
 			case 0:
-				header.cmd = 1;
-				if (wu_hdr_len() == 0) {
-					u32 offset = wu_hdr_offset();
-					header.len_16 = sizeof(Preset) - offset;
-					header.offset_16 = offset;
-					header.magic[3] = magic[3]; // 16 bit mode
-				}
-				set_state(WU_SND_HDR, (u8*)&header, is_wu_hdr_32bit() ? 14 : 10);
+				set_state(SEND_HEADER);
 				break;
 			// request to save
 			case 1:
-				load_preset(header.idx);
-				set_state(WU_RCV_DATA, ((u8*)&cur_preset) + wu_hdr_offset(), wu_hdr_len());
+				set_state(RCV_DATA);
 				break;
 			}
 			break;
 		// finished receiving data
-		case WU_RCV_DATA:
-			if (header.cmd == 1 && header.idx < NUM_PRESETS)
-				log_ram_edit(SEG_PRESET);
+		case RCV_DATA:
+			log_ram_edit(SEG_PRESET);
+			save_preset();
 			web_editor_reset();
 			break;
-		// we sent the header, now send the data
-		case WU_SND_HDR:
-			u8* data = header.idx == ram_preset_id ? (u8*)&cur_preset : (u8*)preset_flash_ptr(header.idx);
-			set_state(WU_SND_DATA, data + wu_hdr_offset(), wu_hdr_len());
+		// finished sending header
+		case SEND_HEADER:
+			set_state(SEND_DATA);
 			break;
-		// done sending data
-		case WU_SND_DATA:
+		// finished sending data
+		case SEND_DATA:
 			web_editor_reset();
 			break;
 		default:
--- sw/Core/Src/plinky/usb/web_editor.h
+++ sw/Core/Src/plinky/usb/web_editor.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "utils.h"
 
+extern u8 receiving_web_preset;
+
 void web_editor_reset(void);
 void web_editor_frame(void);
