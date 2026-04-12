bbf9c7941760f839cc0f623af247df59f0347adf
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2026-04-12 22:11:57 +0200

    Remove unused lunr stemmers

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 cd "$(dirname "$0")/.."
 
 mkdocs build
+rm -rf site/assets/javascripts/lunr/min site/assets/javascripts/lunr/tinyseg.js site/assets/javascripts/lunr/wordcut.js
 rsync -a --delete --exclude='markdown/' site/ docs/
 rm -rf site/
